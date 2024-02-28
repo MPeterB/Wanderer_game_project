697bb575dce1006b7b7af782fb2d32ad629919b0
MPeterB <mozsipeter@gmail.com>
2024-02-28 17:01:53 +0100

    fix ctx type

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css';
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
-const ctx: any = canvas.getContext('2d');
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
 ctx.fillRect(10, 10, 100, 100);
