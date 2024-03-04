f1d0a25fb2c039f5e16ab6483e1293c8e09890d5
MPeterB <mozsipeter@gmail.com>
2024-03-04 15:16:59 +0100

    change tile type

--- src/game/tiles.ts
+++ src/game/tiles.ts
@@ -1,14 +1,11 @@
 export type tile = {
   image: HTMLImageElement;
-  occupied: boolean;
 };
 
 export const wall: tile = {
   image: document.getElementById('wall') as HTMLImageElement,
-  occupied: true,
 };
 
 export const floor: tile = {
   image: document.getElementById('floor') as HTMLImageElement,
-  occupied: false,
 };
