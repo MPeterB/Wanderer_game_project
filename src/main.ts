01a4b3a8007e1a904e38bd572799267ebf57404a
MPeterB <mozsipeter@gmail.com>
2024-03-09 12:01:52 +0100

    add new event listener

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import './style.css';
 import { drawMap, map } from './game/map.ts';
-import { ctxH, onKeyPress, heroDown } from './game/hero.ts';
+import { ctxH, heroMove, heroDown } from './game/hero.ts';
 import { drawEnemies } from './game/enemy.ts';
 import { showHeroStats } from './game/showStats.ts';
+import { heroStrike } from './game/battle.ts';
 
 window.onload = (): void => {
-  document.body.addEventListener('keydown', onKeyPress);
+  document.body.addEventListener('keydown', heroMove);
+  document.body.addEventListener('keydown', heroStrike);
   drawMap(map);
   ctxH.drawImage(heroDown, 0, 0, 50, 50);
   showHeroStats();
