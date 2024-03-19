d72b549dabc5ef3a6cb6e38b549e4a84c94d8b55
MPeterB <mozsipeter@gmail.com>
2024-03-19 13:54:32 +0100

    modify randomNumberMinMax()

--- src/game/game.ts
+++ src/game/game.ts
@@ -8,10 +8,11 @@ export const gameProgress: GameProgress = {
   inProgress: false,
 };
 
-export function randomNumberMinMax(min: number, max: number) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+export function randomNumberMinMax(min: number, max: number): number {
+  const minCeiled: number = Math.ceil(min);
+  const maxFloored: number = Math.floor(max);
+  const randomNumber: number = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  return randomNumber;
 }
 
 export function startGame() {
