f5cdc5fbf35ed02ac47f6e641c2a1690493a3f3d
MPeterB <mozsipeter@gmail.com>
2024-03-21 15:30:42 +0100

    hero restores some health if level is won

--- src/game/game.ts
+++ src/game/game.ts
@@ -24,12 +24,42 @@ export function randomNumberMinMax(min: number, max: number): number {
   return randomNumber;
 }
 
+function heroHealthRestore(): void {
+  const chances: number[] = [1, 2, 2, 2, 2, 3, 3, 3, 3, 3];
+  const chosenIndex: number = chances[randomNumberMinMax(0, 9)];
+  let maxRestorable: number = 0;
+  switch (chosenIndex) {
+    case 1:
+      heroCurrent.currentHealth = heroCurrent.maxHealth;
+      break;
+    case 2:
+      maxRestorable = heroCurrent.maxHealth / 3;
+      if ((heroCurrent.currentHealth + maxRestorable) > heroCurrent.maxHealth) {
+        heroCurrent.currentHealth = heroCurrent.maxHealth;
+      } else {
+        heroCurrent.currentHealth = (heroCurrent.currentHealth + maxRestorable);
+      }
+      break;
+    case 3:
+      maxRestorable = heroCurrent.maxHealth * 0.1;
+      if ((heroCurrent.currentHealth + maxRestorable) > heroCurrent.maxHealth) {
+        heroCurrent.currentHealth = heroCurrent.maxHealth;
+      } else {
+        heroCurrent.currentHealth = (heroCurrent.currentHealth + maxRestorable);
+      }
+      break;
+    default:
+      heroCurrent.currentHealth += 1;
+  }
+}
+
 export function winLevel() {
   const wonLevelScreen = document.getElementById('wonLevelScreen') as HTMLElement;
   const alertBox = document.getElementById('alertBox') as HTMLElement;
   const statsContainer = document.getElementById('stats-container') as HTMLElement;
   wonLevel.value = true;
   heroCurrent.moving = false;
+  heroHealthRestore();
   levelUpEnemies();
   setTimeout(() => {
     wonLevelScreen.style.display = 'block';
